b8a6427ef95a1c1002b41530d9d991b5d5e38c22
Drew Hutton <drew.business2308@gmail.com>
2019-08-22 10:56:31 +0930

    Add back support for service account activation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import fs from 'fs';
 
 // TODO: Support mutiple operating systems
 
@@ -7,6 +8,7 @@ async function run() {
   try {
     // Inputs
     const gcloud_channel = core.getInput('gcloud_channel');
+    const service_account = core.getInput('service_account');
 
     // Start Installing
     core.debug(`Installing GCloud snap channel ${gcloud_channel}`);
@@ -20,6 +22,9 @@ async function run() {
     await exec.exec('sudo chown root:root /');
 
     // Initialize Gcloud
+    const SABuffer = new Buffer(service_account, 'base64');
+    fs.writeFileSync('gcloud.json', SABuffer);
+
     await exec.exec(
       'gcloud auth activate-service-account --key-file=gcloud.json',
     );
